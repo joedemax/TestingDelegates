990fb71bd81b01f8c5206f3bad2ac8181f3541bc
joedemax <joedemax@me.com>
2017-07-21 05:50:16 +0100

    Add project files.

---
+++ TestingDelegates/CounterClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TestingDelegates
+{
+    class CounterClass
+    {
+        TimerTick tick;
+
+        //Constructor
+        public CounterClass(TimerTick tick)
+        {
+            Thread counter = new Thread(new ThreadStart(CounterThread));
+            this.tick = tick;
+            counter.Start();
+        }
+
+        private void CounterThread()
+        {
+            int counter = 0;
+
+            while(true)
+            {
+                Console.WriteLine(counter);
+                counter++;
+                Thread.Sleep(1000);
+                tick(counter);
+            }
+        }
+
+    }
+}
---
+++ TestingDelegates/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TestingDelegates
+{
+    public delegate void LabelDelegate(int y);
+    public delegate void TimerTick(int i);
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            TimerTick tick = new TimerTick(UpdateLabel);
+            CounterClass counterClass = new CounterClass(tick);
+            tick += test2;
+            Console.WriteLine("wtf");
+        }
+
+
+        private void UpdateLabel(int y)
+        {
+            if(label1.InvokeRequired)
+            {
+                LabelDelegate labelDelegate = new LabelDelegate(UpdateLabel);
+                label1.Invoke(labelDelegate, y);
+            }
+            label1.Text = y.ToString();
+        }
+
+        private void test(int i)
+        {
+            Console.WriteLine("Test {0}", i);
+        }
+
+        private void test2(int i)
+        {
+            Console.WriteLine("Test 2 {0}", i);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
